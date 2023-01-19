7c82a385c3b2abee751c8a6c87809e68547cd113
Tony Li Laptop <litoj2001@gmail.com>
2023-01-19 14:40:43 -0800

    updated with stuff

--- encrypt.py
+++ encrypt.py
@@ -1,22 +1,9 @@
 from Crypto.Cipher import AES
-import random, string
 
-# gives you a random key of size bytes
-def get_rand_key(size):
-    return ''.join(random.choice(string.ascii_letters) for i in range(size))
+key = b"0123456789012345"
+aesCipher = AES.new(key, AES.MODE_ECB)
+blockToEncrypt = b"ABCDEFGHIJKLMNOP"
 
-# 128 bits = 16B
-def pad_128_bits(message):
-    offset = 16 - len(message) % 16
-    for i in range(offset):
-        message = message + chr(offset)
-    return message
+blockToDecrypt = aesCipher.encrypt(blockToEncrypt)
 
-# initialize cipher
-key = get_rand_key(16)
-cipher = AES.new(key, AES.MODE_ECB)
-print(key)
-
-print(pad_128_bits("hello wolrd"))
-# msg = cipher.encrypt('this is the plaintext')
-# print (type(msg))
+aesCipher.decrypt(blockToDecrypt)
