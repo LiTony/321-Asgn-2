c2261a8168ab4219f5c2b58ab4b2abd4b37ba0ef
Tony Li Laptop <litoj2001@gmail.com>
2023-01-19 14:41:51 -0800

    readding padding function

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,12 @@
 from Crypto.Cipher import AES
 
+# 128 bits = 16B
+def pad_128_bits(message):
+    offset = 16 - len(message) % 16
+    for i in range(offset):
+        message = message + chr(offset)
+    return message
+
 key = b"0123456789012345"
 aesCipher = AES.new(key, AES.MODE_ECB)
 blockToEncrypt = b"ABCDEFGHIJKLMNOP"
