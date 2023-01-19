1d1bcb436e9fcadd1c167ad5b3c69a5ce12d7b15
Xiuyuan Qiu <xiuyuan206@gmail.com>
2023-01-19 15:59:20 -0800

    added ecb encryption

--- encrypt.py
+++ encrypt.py
@@ -7,10 +7,29 @@ def pad_128_bits(message):
         message = message + chr(offset)
     return message
 
+# returns a list of 16B chunks, padded
+def ecb(message, aes):
+    pad_m = pad_128_bits(message)
+    count = len(pad_m)//16 
+    
+    # create a list of 16B chunks
+    chunks = []  
+    for i in range(count):
+        start = i * 16
+        chunk = pad_m[start:start+16]
+        chunks.append(bytes(chunk, encoding="ascii"))
+    
+    # encrypt the message, chunk by chunk
+    encrypted = b""
+    for chunk in chunks:
+        encrypted = encrypted + aes.encrypt(chunk)
+    return encrypted
+
+
 key = b"0123456789012345"
 aesCipher = AES.new(key, AES.MODE_ECB)
-blockToEncrypt = b"ABCDEFGHIJKLMNOP"
-
-blockToDecrypt = aesCipher.encrypt(blockToEncrypt)
-
-aesCipher.decrypt(blockToDecrypt)
+plaintext = "hello! my message is longer than 128 bytes :)"
+print(plaintext)
+ciphertext = ecb(plaintext, aesCipher)
+print(ciphertext)
+print(aesCipher.decrypt(ciphertext))
