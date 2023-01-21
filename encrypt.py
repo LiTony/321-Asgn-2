41eee99e62e0d36b4e85a8e6c48f860966874356
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-01-20 21:27:04 -0800

    finished cbc implementation

--- encrypt.py
+++ encrypt.py
@@ -11,9 +11,9 @@ def pad_128_bits(message):
         message = message + bytes(offset)
     return message
 
-# returns a list of 16B chunks, padded
-# message is in byte form
-def ecb(message, aes):
+# pads the message and returns a 
+# list of 16B chunks
+def pad_and_chunk(message):
     pad_m = pad_128_bits(message)
     count = len(pad_m)//16 
 
@@ -23,12 +23,40 @@ def ecb(message, aes):
         start = i * 16
         chunk = pad_m[start:start+16]
         chunks.append(chunk)
+    return chunks
+
+# returns an encrypted, padded set of bytes
+# message is in byte form
+def ecb(message, aes):
+    chunks = pad_and_chunk(message)
     
     # encrypt the message, chunk by chunk
     encrypted = b""
     for chunk in chunks:
         encrypted = encrypted + aes.encrypt(chunk)
     return encrypted
+
+# returns encrypted message by cbc
+# iv is the same size as a 16B chunk
+def cbc(message, iv, aes):
+    chunks = pad_and_chunk(message)
+
+    encrypted = b""
+    prev_cipher = iv
+    for chunk in chunks:
+        xor_chunk = xor(prev_cipher, chunk)
+        curr_cipher = aes.encrypt(xor_chunk)
+        encrypted = encrypted + curr_cipher
+        prev_cipher = curr_cipher
+    return encrypted
+
+# workaround for xor-ing bytes
+def xor(b1, b2):
+    res = b""
+    for b1, b2 in zip(b1, b2):
+        res = res + bytes([b1^b2])
+    return res
+
 
 # returns the byte form of a given image
 def get_image_bytearray(path):
@@ -42,12 +70,16 @@ def showimage(bytes):
     img = Image.open(BytesIO(bytes))
     img.show()
 
+# 16B key and initialization vector
 key = b"0123456789012345"
+iv = b"0123456789012345"
 aesCipher = AES.new(key, AES.MODE_ECB)
 plaintext = get_image_bytearray("mustang.bmp")
 
 # the header is preserved so we can actually view the image
 header, image = plaintext[0:54], plaintext[54:]
-ciphertext = ecb(image, aesCipher)
-showimage(header + ciphertext)
-aesCipher.decrypt(ciphertext)
+ecb_cipher = ecb(image, aesCipher)
+showimage(header + ecb_cipher)
+
+cbc_cipher = cbc(image, iv, aesCipher)
+showimage(header + cbc_cipher)
