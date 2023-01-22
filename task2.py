62776524cb490696e837fa926f6dcce50e03cba4
Tony Li Laptop <litoj2001@gmail.com>
2023-01-22 02:11:56 -0800

    task 2 foundations

--- task2.py
+++ task2.py
@@ -3,11 +3,49 @@ from PIL import Image
 from io import BytesIO
 import urllib.parse
 
-def submit():
+from encrypt import pad_and_chunk, cbc;
+
+def testCode():
+    def submitTest():
+        userAnswer = input("Type an arbitrary string: ")
+        userEncode = urllib.parse.quote(userAnswer, safe='~()*!.\'')
+        result = b"userid=456; userdata=" + userEncode.encode('ascii') + b";session-id=31337"
+        chunks = pad_and_chunk(result)
+        return chunks
+
+    chunks = submitTest()
+    for chunk in chunks:
+        print(chunk, f"\t len = %d" %(len(chunk)))
+    return
+
+def submit(iv, aes):
     userAnswer = input("Type an arbitrary string: ")
     userEncode = urllib.parse.quote(userAnswer, safe='~()*!.\'')
-    result = "userid=456; userdata=" + userEncode + ";session-id=31337"
-    return result
+    result = b"userid=456; userdata=" + userEncode.encode('ascii') + b";session-id=31337"
+    print("result:\n\t", result)
+    encrypted = cbc(result, iv, aes)
+    print("encrypted:\n\t", encrypted)
+    return encrypted
 
-answer = submit()
-print(answer)
+def main():
+    # 16B key and initialization vector
+    key = b"0123456789012345"
+    iv = b"0123456789012345"
+    aesCipher = AES.new(key, AES.MODE_ECB)
+    
+    encrypted = submit(iv, aesCipher)
+
+    decrypted = b""
+
+    # CONSTRUCTION IN PROGRESS
+    len16 = len(encrypted) // 16
+    for i in range(len16):
+        decrypted += aesCipher.decrypt(encrypted[(i-1)*16:i*16])
+    # CONSTRUCTION IN PROGRESS
+
+    print("decrypted:\n\t", decrypted)
+
+    return
+
+if __name__ == "__main__":
+    main()
